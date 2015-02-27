d19cbc391aa07e78055577dd5e7212b94d6b9eec
pedmiston <pierce.edmiston@gmail.com>
2015-02-27 14:19:04 -0600

    Tidied up

--- exp/spatial-cueing.py
+++ exp/spatial-cueing.py
@@ -1,4 +1,4 @@
-import random
+from numpy.random import choice
 
 from psychopy import visual, core
 from psychopy.data import StairHandler
@@ -10,8 +10,9 @@ from util.screenstates import TargetDetection
 class SpatialCueing(ioHubExperimentRuntime):
 
     def run(self, *args, **kwargs):
-        self.running = True 
+        self.running = True
 
+        # visuals
         display = self.hub.devices.display
         self.window = visual.Window(display.getPixelResolution(),
                 monitor = display.getPsychopyMonitorName(),
@@ -19,61 +20,66 @@ class SpatialCueing(ioHubExperimentRuntime):
                 fullscr = True, allowGUI = False,
                 screen = display.getIndex())
 
-
+        # devices
         self.keyboard = self.hub.devices.keyboard
+
+        ## for advancing the screen past the instructions
         advance = DeviceEventTrigger(device = self.keyboard,
                 event_type = EventConstants.KEYBOARD_PRESS,
                 event_attribute_conditions = {'key': ' '})
+        ## for trying to quit the experiment
         quit = DeviceEventTrigger(device = self.keyboard,
                 event_type = EventConstants.KEYBOARD_PRESS,
                 event_attribute_conditions = {'key': 'q'},
                 trigger_function = self.request_quit)
+        ## for responding if the target was present or absent
+        response_keys = {'y': 'present', 'n': 'absent'}
+        responder = DeviceEventTrigger(device = self.keyboard,
+                event_type = EventConstants.KEYBOARD_PRESS,
+                event_attribute_conditions = {'key': response_keys.keys()})
+
+        # screens
         instructions = InstructionScreen(self, timeout = 1 * 60.0,
                 eventTriggers = [advance, quit],
                 text = "Press SPACEBAR to advance, or press 'q' to quit.")
+        detect_target = TargetDetection(self,
+                eventTriggers = [responder, quit])
 
-	response_keys = {'y': 'present', 'n': 'absent'}
-        responder = DeviceEventTrigger(device = self.keyboard,
-		event_type = EventConstants.KEYBOARD_PRESS,
-		event_attribute_conditions = {'key': response_keys.keys()})
-	detect_target = TargetDetection(self, 
-		eventTriggers = [responder, quit])
-
+        # Show instructions
+        # -----------------
         instructions.switchTo()
-	if not self.running: return
 
+        if not self.running:
+            return
+
+        # Calibrate opacity
+        # -----------------
         staircase = StairHandler(0.5, stepSizes = 0.1,
                 nTrials = 10, nUp = 2, nDown = 2, stepType = 'lin',
                 minVal = 0.0, maxVal = 1.0)
 
-        for current_opacity in staircase:
-            # first arg is start time, which we don't care about
-	    opacity = staircase.next()
-	    print "Using opacity:", opacity
-	    
-	    # will want to update this to control percentages
-	    present = random.choice(['present', 'absent'])
+        for opacity in staircase:
+            present = choice(['present', 'absent'], p = [0.8, 0.2])
 
-	    if present == 'present':
-		location_name = random.choice(['left', 'right'])
-	    else:
-		location_name = None
-	    
-            _, rt, event = detect_target.switchTo(
-			opacity = opacity, location_name = location_name)
+            if present == 'present':
+                location_name = choice(['left', 'right'])
+            else:
+                location_name = None
 
-	    if not self.running: break
+            _, rt, event = detect_target.switchTo(opacity, location_name)
 
-	    response = response_keys[event.key]
-	    graded = (response == present)
-	    print "Grade:", graded
-	    staircase.addResponse(graded)
-	    core.wait(1.0)
+            if not self.running:
+                break
+
+            response = response_keys[event.key]
+            graded = (response == present)
+            staircase.addResponse(graded)
+            core.wait(1.0)
 
     def request_quit(self, *args, **kwargs):
         """ User requested to quit the experiment. """
         self.running = False
-	print "Quit requested..."
+        print "Quit requested..."
         return True  # exits the screenstate
 
 if __name__ == '__main__':
--- exp/util/screenstates.py
+++ exp/util/screenstates.py
@@ -1,6 +1,6 @@
 from psychopy import visual
 from psychopy.iohub import ioHubExperimentRuntime, EventConstants
-from psychopy.iohub.util import (ScreenState, TimeTrigger, 
+from psychopy.iohub.util import (ScreenState, TimeTrigger,
                                  DeviceEventTrigger)
 
 from util.dynamicmask import DynamicMask
@@ -18,76 +18,78 @@ class TargetDetection(ScreenState):
     """
     def __init__(self, experimentRuntime, eventTriggers):
         super(TargetDetection, self).__init__(experimentRuntime, timeout = 10.0,
-		eventTriggers = eventTriggers)
+                eventTriggers = eventTriggers)
 
         window = experimentRuntime.window
 
-	gutter = -300  # x-distance between left and right locations
-	self.location_map = {'left': (-gutter, 0), 'right': (gutter, 0)}
+        gutter = 300  # distance from centroid to left/right locations
+        left = (-gutter, 0)
+        right = (gutter, 0)
+        self.location_map = {'left': left, 'right': right}
+
         mask_kwargs = {'win': window, 'size': [200, 200]}
         masks = {}
-        masks['left']  = DynamicMask(pos = self.location_map['left'], 
-			**mask_kwargs)
-        masks['right'] = DynamicMask(pos = self.location_map['right'],
-			**mask_kwargs)
+        masks['left']  = DynamicMask(pos = left, **mask_kwargs)
+        masks['right'] = DynamicMask(pos = right, **mask_kwargs)
         self.stim.update(masks)
         self.stimNames.extend(['left', 'right'])
 
         fix = visual.TextStim(window, text = '+', height = 40,
-			font = 'Consolas', color = 'black')
+                font = 'Consolas', color = 'black')
         self.stim.update({'fix': fix})
         self.stimNames.append('fix')
 
-        target = visual.Circle(window, radius = 10, pos = (-300, 0), 
-			fillColor = 'white', opacity = 0.0)
+        target = visual.Circle(window, radius = 10, pos = (-300, 0),
+                fillColor = 'white', opacity = 0.0)
         self.stim.update({'target': target})
         self.stimNames.append('target')
 
-	refresh = TimeTrigger(start_time = self.interval,
-			delay = REFRESH_RATE,
-			repeat_count = -1,
-			trigger_function = self.refresh)
-	self.addEventTrigger(refresh)
-	self.last_frame = None
+        self.last_frame = None
+        refresh = TimeTrigger(start_time = self.interval,
+                delay = REFRESH_RATE, repeat_count = -1,
+                trigger_function = self.refresh)
+        self.addEventTrigger(refresh)
 
-	self.target_opacity = None  # will be set on switch
-	target_onset = 1.0 # TEMPORARY
-	onset = TimeTrigger(start_time = self.getStateStartTime,
-			delay = target_onset, repeat_count = 1,
-			trigger_function = self.reveal)
-	self.addEventTrigger(onset)
+        self.target_opacity = None  # will be set on switch
+        target_onset = 1.0          # TEMPORARY
+        onset = TimeTrigger(start_time = self.getStateStartTime,
+                delay = target_onset, repeat_count = 1,
+                trigger_function = self.reveal)
+        self.addEventTrigger(onset)
 
     def interval(self):
-	""" Return the time of the last flip.
+        """ Return the time of the last flip.
 
-	For the first interval, start when the ScreenState flips. For subsequent
-	intervals, return the last_frame variable which is updated when the 
-	screen is rebuilt.
-	"""
-	if self.last_frame == None:
-	    self.last_frame = self.getStateStartTime()
-	return self.last_frame
+        For the first interval, start when the ScreenState flips. For subsequent
+        intervals, return the last_frame variable which is updated when the
+        screen is rebuilt.
+        """
+        if self.last_frame == None:
+            self.last_frame = self.getStateStartTime()
+        return self.last_frame
 
     def refresh(self, *args, **kwargs):
-	""" Triggered when it's been REFRESH_RATE since last flip. """
-	self.dirty = True
-	self.last_frame = self.flip()
-	return False
+        """ TimeTriggered when it's been REFRESH_RATE since last_frame. """
+        self.dirty = True
+        self.last_frame = self.flip()
+        return False
 
     def reveal(self, *args, **kwargs):
-	""" Triggered when it's been target_onset since switching. """
-	self.stim['target'].setOpacity(self.target_opacity)
-	self.refresh()
+        """ TimeTriggered when it's been target_onset since screen start. """
+        self.stim['target'].setOpacity(self.target_opacity)
+        self.refresh()
 
     def switchTo(self, opacity, location_name):
-        """ Set the target opacity and run the trial """
-	if location_name: # target present trial
-	    self.target_opacity = opacity
-	    location = self.location_map[location_name]
-	else:        # target absent trial
-	    self.target_opactiy = 0.0
-	    location = (0, 0)
+        """ Set the target opacity and run the trial. """
+        if location_name:
+            # target present trial
+            self.target_opacity = opacity
+            location = self.location_map[location_name]
+        else:
+            # target absent trial
+            self.target_opacity = 0.0
+            location = (0, 0)
 
-	self.stim['target'].setPos(location)
-	self.stim['target'].setOpacity(0.0)  # start with target hidden
-	return super(TargetDetection, self).switchTo()
+        self.stim['target'].setPos(location)
+        self.stim['target'].setOpacity(0.0)  # start with target hidden
+        return super(TargetDetection, self).switchTo()
