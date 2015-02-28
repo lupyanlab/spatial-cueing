8dbf5e0887a249e98612d3800d45fd414a94d93b
pedmiston <pierce.edmiston@gmail.com>
2015-02-28 07:46:05 -0600

    Plot changes in opacity as well

--- exp/spatial-cueing/analysis/view_calibrations.R
+++ exp/spatial-cueing/analysis/view_calibrations.R
@@ -10,6 +10,11 @@ calibrations <- compile("calibration", key = "SPC",
 calibrations <- calibrations %>% group_by(subj_code) %>%
   mutate(rolling = rollmean(accuracy, k = 5, fill = NA))
 
+# should converge
 ggplot(calibrations, aes(x = trial_ix, y = rolling, group = subj_code)) +
   geom_line(aes(color = subj_code)) +
   geom_hline(yintercept = 0.6, lty = 2)
+
+# doesn't need to converge, just needs to stabilize
+ggplot(calibrations, aes(x = trial_ix, y = opacity, group = subj_code)) +
+  geom_line(aes(color = subj_code))
