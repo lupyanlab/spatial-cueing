d36a69950db4f9680939aa65b1ac4fa715e8c558
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-10-26 13:46:30 -0500

    Compile project reports as vignettes

--- data-raw/compiler.R
+++ data-raw/compiler.R
@@ -20,21 +20,21 @@ code_responses_as_binary <- function(response) {
   recode(response, "'go'=1; 'nogo'=0", as.numeric.result = TRUE)
 }
 
-get_spatial_cueing <- function(interval = 0.750, flicker = "on") {  
+get_spatial_cueing <- function(interval = 0.750, flicker = "on") {
   # compile data files matching correct_pattern and add columns for between
   # subjects variables interval and flicker
   compile_experiment <- function(regex_pattern, interval, flicker) {
-    spatial_cueing <- compile(data_directory = "data/iohub", pattern = as.character(regex_pattern))
+    spatial_cueing <- compile(data_directory = "data-raw/go-nogo", pattern = as.character(regex_pattern))
     spatial_cueing <- mutate(spatial_cueing, interval = interval, flicker = flicker)
     spatial_cueing
   }
 
   pattern_matcher <- data_frame(
-    interval = c(0.750, 0.750, 0.100), 
+    interval = c(0.750, 0.750, 0.100),
     flicker = c("on", "off", "on"),
     regex_pattern = c("SPC3?_3", "SPC4", "SPC5")
   )
-  correct_patterns <- pattern_matcher[(pattern_matcher$interval %in% interval & 
+  correct_patterns <- pattern_matcher[(pattern_matcher$interval %in% interval &
                                        pattern_matcher$flicker %in% flicker), ]
 
   print('Compiling raw data...')
@@ -42,30 +42,39 @@ get_spatial_cueing <- function(interval = 0.750, flicker = "on") {
     split(.$regex_pattern) %>%
     map(~ compile_experiment(.$regex_pattern, .$interval, .$flicker)) %>%
     rbind_all(.)
-  
+
   # handle exceptions
   spatial_cueing[spatial_cueing$subj_id %in% c("SPC504a", "SPC508"), "interval"] = 0.10
-  
+
   # SPC6 is weird...
   # The last column, interval, is unlabeled, which causes all colnames
   # to be off by one (and the first column is named "row.names")
-  # The data all compiles though, so just compile it, and shift the 
+  # The data all compiles though, so just compile it, and shift the
   # column names before merging with the rest of the data
   spc6 <- compile_experiment("SPC6", interval = 0.1, flicker = "off")
   spc6 <- select(spc6, -interval, -flicker)
-  
+
   new_colnames <- c(colnames(spc6)[2:length(colnames(spc6))], "interval")
   colnames(spc6) <- new_colnames
   spc6$flicker <- "off"
-  
+
   spatial_cueing <- rbind_list(spatial_cueing, spc6)
 
   print('Coding variables...')
   spatial_cueing$cue_type <- code_missing_cue_type_as_nocue(spatial_cueing$cue_type)
   spatial_cueing$response_b <- code_responses_as_binary(spatial_cueing$response)
-  
+
   print('Dropping practice trials...')
   cueing <- filter(spatial_cueing, part != "practice")
-  
+
   cueing
 }
+
+go_nogo <- get_spatial_cueing(interval = c(0.750, 0.100), flicker = c("on", "off"))
+# devtools::use_data(go_nogo)
+
+twomask <- compile("data-raw/twomask", pattern = "SPC", sep = ",")
+# devtools::use_data(twomask, overwrite = TRUE)
+
+fourmask_longsoa <- compile("data-raw/fourmask-longsoa", pattern = "P", sep = ",")
+# devtools::use_data(fourmask_longsoa, overwrite = TRUE)
